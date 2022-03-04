41c1d9f9fc18fa1daf8a2f5b68523b6a56fc2464
JusBar206 <justinbartley206@gmail.com>
2022-03-04 14:35:04 -0800

    guess rthe word game

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const guessedLettersBox = document.querySelector(".guessed-letters");
+const guessButton = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuesses = document.querySelector(".remaining");
+const remainingGuessesSpan = document.querySelector(".remaining span");
+const message = document.querySelector(".message");
+const PlayAgainButton = document.querySelector(".play-again");
+
+const word = "magnolia";
+
+
+// display symbols as placeholders for the chosen words letters
+
+const placeholder = function(word) {
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("●")
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholder(word);
+
+guessButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const guess = letterInput.value;
+    console.log(guess);
+    letterInput.value = "";
+});
