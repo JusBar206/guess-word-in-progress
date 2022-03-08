9895a0f1a590472d6f5304f94873d05c1667d155
JusBar206 <justinbartley206@gmail.com>
2022-03-08 11:54:54 -0800

    guess word game

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const message = document.querySelector(".message");
 const PlayAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
-
+const guessedLetters = [];
 
 // display symbols as placeholders for the chosen words letters
 
@@ -25,7 +25,36 @@ placeholder(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
     const guess = letterInput.value;
-    console.log(guess);
+    const goodGuess = validateInput(guess);
+  
+    if (goodGuess) {
+      makeGuess(guess);
+    }
     letterInput.value = "";
-});
+  });
+
+
+const validateInput = function(input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = "please enter A letter.";
+    } else if (input.length > 1) {
+        message.innerText = "Please enther a single letter.";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter from A-Z.";
+    } else {
+        return input;
+     }
+    };
+
+const makeGuess = function(guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        message.innerText = "You already guessed that letter dumb dumb. Try again";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
